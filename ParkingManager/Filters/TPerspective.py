036161f86b8034d8508c7cce0962091fb7db8b07
esmendozac <41306149+esmendozac@users.noreply.github.com>
2021-12-09 08:48:14 -0500

    -- Creación del entorno de anaconda

--- ParkingManager/Filters/TPerspective.py
+++ ParkingManager/Filters/TPerspective.py
@@ -32,6 +32,8 @@ class TPerspective:
         self.tp_width = 1200
         self.vertical_degrees = 25
         self.horizontal_degrees = 5
+        self.horizontal_tolerance = 15
+        self.vertical_tolerance = 20
 
     def draw_widget(self, row: int, col: int, widget_id: int):
         """
@@ -44,12 +46,24 @@ class TPerspective:
 
         setattr(self.ui, f'tp_group_{widget_id}', QtWidgets.QGroupBox(self.ui.scrollAreaWidgetContents))
         tp_group = getattr(self.ui, f'tp_group_{widget_id}')
-        tp_group.setMinimumSize(QtCore.QSize(0, 90))
+        tp_group.setMinimumSize(QtCore.QSize(0, 150))
         tp_group.setObjectName(f'tp_group_{widget_id}')
+
+        setattr(self.ui, f'tp_btn_clear_{widget_id}', QtWidgets.QPushButton(tp_group))
+        tp_btn_clear = getattr(self.ui, f'tp_btn_clear_{widget_id}')
+        tp_btn_clear.setGeometry(QtCore.QRect(300, 30, 40, 40))
+        tp_btn_clear.setMinimumSize(QtCore.QSize(40, 40))
+        tp_btn_clear.setMaximumSize(QtCore.QSize(40, 40))
+        tp_btn_clear.setText("")
+        icon_tp_btn_clear = QtGui.QIcon()
+        icon_tp_btn_clear.addPixmap(QtGui.QPixmap("icons/limpiar.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        tp_btn_clear.setIcon(icon_tp_btn_clear)
+        tp_btn_clear.setIconSize(QtCore.QSize(32, 32))
+        tp_btn_clear.setObjectName(f'tp_btn_clear_{widget_id}')
 
         setattr(self.ui, f'tp_btn_start_{widget_id}', QtWidgets.QPushButton(tp_group))
         tp_btn_start = getattr(self.ui, f'tp_btn_start_{widget_id}')
-        tp_btn_start.setGeometry(QtCore.QRect(10, 30, 40, 40))
+        tp_btn_start.setGeometry(QtCore.QRect(220, 30, 40, 40))
         tp_btn_start.setMinimumSize(QtCore.QSize(40, 40))
         tp_btn_start.setMaximumSize(QtCore.QSize(40, 40))
         tp_btn_start.setStyleSheet("border-color: rgb(255, 85, 0);")
@@ -60,13 +74,168 @@ class TPerspective:
         tp_btn_start.setIconSize(QtCore.QSize(36, 36))
         tp_btn_start.setObjectName(f'tp_btn_start_{widget_id}')
 
+        setattr(self.ui, f'tp_btn_view_{widget_id}', QtWidgets.QPushButton(tp_group))
+        tp_btn_view = getattr(self.ui, f'tp_btn_view_{widget_id}')
+        tp_btn_view.setGeometry(QtCore.QRect(260, 30, 40, 40))
+        tp_btn_view.setMinimumSize(QtCore.QSize(40, 40))
+        tp_btn_view.setMaximumSize(QtCore.QSize(40, 40))
+        tp_btn_view.setText("")
+        icon_tp_btn_view = QtGui.QIcon()
+        icon_tp_btn_view.addPixmap(QtGui.QPixmap("icons/ver.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        tp_btn_view.setIcon(icon_tp_btn_view)
+        tp_btn_view.setIconSize(QtCore.QSize(32, 32))
+        tp_btn_view.setObjectName(f'tp_btn_view_{widget_id}')
+
+        setattr(self.ui, f'tp_btn_delete_{widget_id}', QtWidgets.QPushButton(tp_group))
+        tp_btn_delete = getattr(self.ui, f'tp_btn_delete_{widget_id}')
+        tp_btn_delete.setGeometry(QtCore.QRect(340, 30, 40, 40))
+        tp_btn_delete.setMinimumSize(QtCore.QSize(40, 40))
+        tp_btn_delete.setMaximumSize(QtCore.QSize(40, 40))
+        tp_btn_delete.setText("")
+        icon_tp_btn_delete = QtGui.QIcon()
+        icon_tp_btn_delete.addPixmap(QtGui.QPixmap("icons/eliminar.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        tp_btn_delete.setIcon(icon_tp_btn_delete)
+        tp_btn_delete.setIconSize(QtCore.QSize(32, 32))
+        tp_btn_delete.setObjectName(f'tp_btn_delete_{widget_id}')
+
+        setattr(self.ui, f'tp_sb_v_lines_{widget_id}', QtWidgets.QSpinBox(tp_group))
+        tp_sb_v_lines = getattr(self.ui, f'tp_sb_v_lines_{widget_id}')
+        tp_sb_v_lines.setGeometry(QtCore.QRect(80, 40, 42, 20))
+        tp_sb_v_lines.setMinimum(1)
+        tp_sb_v_lines.setMaximum(40)
+        tp_sb_v_lines.setProperty("value", 20)
+        tp_sb_v_lines.setObjectName(f'tp_sb_v_lines_{widget_id}')
+
+        setattr(self.ui, f'tp_sb_h_lines_{widget_id}', QtWidgets.QSpinBox(tp_group))
+        tp_sb_h_lines = getattr(self.ui, f'tp_sb_h_lines_{widget_id}')
+        tp_sb_h_lines.setGeometry(QtCore.QRect(20, 40, 42, 20))
+        tp_sb_h_lines.setMinimum(1)
+        tp_sb_h_lines.setMaximum(40)
+        tp_sb_h_lines.setProperty("value", 15)
+        tp_sb_h_lines.setObjectName(f'tp_sb_h_lines_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_h_lines_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_h_lines = getattr(self.ui, f'tp_lbl_h_lines_{widget_id}')
+        tp_lbl_h_lines.setGeometry(QtCore.QRect(10, 40, 31, 16))
+        tp_lbl_h_lines.setObjectName(f'tp_lbl_h_lines_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_v_lines_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_v_lines = getattr(self.ui, f'tp_lbl_v_lines_{widget_id}')
+        tp_lbl_v_lines.setGeometry(QtCore.QRect(70, 40, 31, 16))
+        tp_lbl_v_lines.setObjectName(f'tp_lbl_v_lines_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_lines_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_lines = getattr(self.ui, f'tp_lbl_lines_{widget_id}')
+        tp_lbl_lines.setGeometry(QtCore.QRect(10, 20, 121, 16))
+        tp_lbl_lines.setObjectName(f'tp_lbl_lines_{widget_id}')
+
+        setattr(self.ui, f'tp_sb_h_degrees_{widget_id}', QtWidgets.QSpinBox(tp_group))
+        tp_sb_h_degrees = getattr(self.ui, f'tp_sb_h_degrees_{widget_id}')
+        tp_sb_h_degrees.setGeometry(QtCore.QRect(20, 100, 42, 20))
+        tp_sb_h_degrees.setMinimum(1)
+        tp_sb_h_degrees.setMaximum(45)
+        tp_sb_h_degrees.setProperty("value", 5)
+        tp_sb_h_degrees.setObjectName(f'tp_sb_h_degrees_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_v_degrees_{widget_id}',  QtWidgets.QLabel(tp_group))
+        tp_lbl_v_degrees = getattr(self.ui, f'tp_lbl_v_degrees_{widget_id}')
+        tp_lbl_v_degrees.setGeometry(QtCore.QRect(70, 100, 31, 16))
+        tp_lbl_v_degrees.setObjectName(f'tp_lbl_v_degrees_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_degrees_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_degrees = getattr(self.ui, f'tp_lbl_degrees_{widget_id}')
+        tp_lbl_degrees.setGeometry(QtCore.QRect(10, 80, 111, 16))
+        tp_lbl_degrees.setObjectName(f'tp_lbl_degrees_{widget_id}')
+
+        setattr(self.ui, f'tp_sb_v_degrees_{widget_id}', QtWidgets.QSpinBox(tp_group))
+        tp_sb_v_degrees = getattr(self.ui, f'tp_sb_v_degrees_{widget_id}')
+        tp_sb_v_degrees.setGeometry(QtCore.QRect(80, 100, 42, 20))
+        tp_sb_v_degrees.setMinimum(1)
+        tp_sb_v_degrees.setMaximum(45)
+        tp_sb_v_degrees.setProperty("value", 25)
+        tp_sb_v_degrees.setObjectName(f'tp_sb_v_degrees_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_h_degrees_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_h_degrees = getattr(self.ui, f'tp_lbl_h_degrees_{widget_id}')
+        tp_lbl_h_degrees.setGeometry(QtCore.QRect(10, 100, 31, 16))
+        tp_lbl_h_degrees.setObjectName(f'tp_lbl_h_degrees_{widget_id}')
+
+        setattr(self.ui, f'tp_sb_w_size_{widget_id}', QtWidgets.QSpinBox(tp_group))
+        tp_sb_w_size = getattr(self.ui, f'tp_sb_w_size_{widget_id}')
+        tp_sb_w_size.setGeometry(QtCore.QRect(310, 100, 51, 20))
+        tp_sb_w_size.setMinimum(100)
+        tp_sb_w_size.setMaximum(1920)
+        tp_sb_w_size.setProperty("value", 1200)
+        tp_sb_w_size.setObjectName(f'tp_sb_w_size_{widget_id}')
+
+        setattr(self.ui, f'tp_sb_h_size_{widget_id}', QtWidgets.QSpinBox(tp_group))
+        tp_sb_h_size = getattr(self.ui, f'tp_sb_h_size_{widget_id}')
+        tp_sb_h_size.setGeometry(QtCore.QRect(229, 100, 51, 20))
+        tp_sb_h_size.setMinimum(100)
+        tp_sb_h_size.setMaximum(1080)
+        tp_sb_h_size.setProperty("value", 500)
+        tp_sb_h_size.setObjectName(f'tp_sb_h_size_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_size_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_size = getattr(self.ui, f'tp_lbl_size_{widget_id}')
+        tp_lbl_size.setGeometry(QtCore.QRect(219, 80, 111, 16))
+        tp_lbl_size.setObjectName(f'tp_lbl_size_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_w_size_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_w_size = getattr(self.ui, f'tp_lbl_w_size_{widget_id}')
+        tp_lbl_w_size.setGeometry(QtCore.QRect(289, 100, 31, 16))
+        tp_lbl_w_size.setObjectName(f'tp_lbl_w_size_{widget_id}')
+
+        setattr(self.ui, f'tp_lbl_h_size_{widget_id}', QtWidgets.QLabel(tp_group))
+        tp_lbl_h_size = getattr(self.ui, f'tp_lbl_h_size_{widget_id}')
+        tp_lbl_h_size.setGeometry(QtCore.QRect(219, 100, 31, 16))
+        tp_lbl_h_size.setObjectName(f'tp_lbl_h_size_{widget_id}')
+
         _translate = QtCore.QCoreApplication.translate
         tp_group.setTitle(_translate("MainWindow", "Transformación de perspectiva"))
+        tp_btn_clear.setToolTip(_translate("MainWindow", "<html><head/><body><p>Limpiar</p></body></html>"))
+        tp_btn_start.setToolTip(_translate("MainWindow", "<html><head/><body><p>Dibujar coordenadas</p></body></html>"))
+        tp_btn_view.setToolTip(_translate("MainWindow", "<html><head/><body><p>Ver imagen</p></body></html>"))
+        tp_btn_delete.setToolTip(_translate("MainWindow", "<html><head/><body><p>Limpiar</p></body></html>"))
+        tp_lbl_h_lines.setText(_translate("MainWindow", "H"))
+        tp_lbl_v_lines.setText(_translate("MainWindow", "V"))
+        tp_lbl_lines.setText(_translate("MainWindow", "Tolerancias entre lineas"))
+        tp_lbl_v_degrees.setText(_translate("MainWindow", "V"))
+        tp_lbl_degrees.setText(_translate("MainWindow", "Tolerancias en grados"))
+        tp_lbl_h_degrees.setText(_translate("MainWindow", "H"))
+        tp_lbl_size.setText(_translate("MainWindow", "Tamaño perspectiva"))
+        tp_lbl_w_size.setText(_translate("MainWindow", "W"))
+        tp_lbl_h_size.setText(_translate("MainWindow", "H"))
 
         self.ui.formLayout.setWidget(widget_id, QtWidgets.QFormLayout.FieldRole, tp_group)
 
         # Conexiones
         tp_btn_start.clicked.connect(lambda callback: self.execute_perspective_transform())
+        tp_sb_h_lines.valueChanged.connect(lambda callback: self.update_parameters())
+        tp_sb_v_lines.valueChanged.connect(lambda callback: self.update_parameters())
+        tp_sb_h_degrees.valueChanged.connect(lambda callback: self.update_parameters())
+        tp_sb_v_degrees.valueChanged.connect(lambda callback: self.update_parameters())
+        tp_sb_h_size.valueChanged.connect(lambda callback: self.update_parameters())
+        tp_sb_w_size.valueChanged.connect(lambda callback: self.update_parameters())
+
+    def update_parameters(self):
+        """
+        Actualización de parametros
+        :return:
+        """
+        tp_sb_h_lines = getattr(self.ui, f'tp_sb_h_lines_{self.widget_id}')
+        tp_sb_v_lines = getattr(self.ui, f'tp_sb_v_lines_{self.widget_id}')
+        tp_sb_h_degrees = getattr(self.ui, f'tp_sb_h_degrees_{self.widget_id}')
+        tp_sb_v_degrees = getattr(self.ui, f'tp_sb_v_degrees_{self.widget_id}')
+        tp_sb_h_size = getattr(self.ui, f'tp_sb_h_size_{self.widget_id}')
+        tp_sb_w_size = getattr(self.ui, f'tp_sb_w_size_{self.widget_id}')
+
+        self.tp_height = int(tp_sb_h_size.value())
+        self.tp_width = int(tp_sb_w_size.value())
+        self.vertical_degrees = int(tp_sb_v_degrees.value())
+        self.horizontal_degrees = int(tp_sb_h_degrees.value())
+        self.horizontal_tolerance = int(tp_sb_h_lines.value())
+        self.vertical_tolerance = int(tp_sb_v_lines.value())
 
     def get_lines(self, transformed, tolerance):
 
@@ -130,16 +299,19 @@ class TPerspective:
 
             tc = np.float32([c[0], c[1], c[3], c[2]])
 
-            print(f"{cont}_Coordenadas inyectadas: {tc}")
+            is_horizontal = True
 
             if TPerspective.is_horizontal(tc):
                 perspective_zone = np.float32([[0, 0], [self.tp_width, 0], [0, self.tp_height], [self.tp_width, self.tp_height]])
                 m = cv2.getPerspectiveTransform(tc, perspective_zone)
+                m_inverse = cv2.getPerspectiveTransform(perspective_zone, tc)
                 transformed = cv2.warpPerspective(self.picture, m, (self.tp_width, self.tp_height))
                 _, binary = cv2.threshold(transformed, 200, 255, cv2.THRESH_BINARY)
             else:
+                is_horizontal = False
                 perspective_zone = np.float32([[0, 0], [self.tp_height, 0], [0, self.tp_width], [self.tp_height, self.tp_width]])
                 m = cv2.getPerspectiveTransform(tc, perspective_zone)
+                m_inverse = cv2.getPerspectiveTransform(perspective_zone, tc)
                 transformed_no_rotated = cv2.warpPerspective(self.picture, m, (self.tp_height, self.tp_width))
                 transformed = np.rot90(transformed_no_rotated, k=1)
                 transformed = transformed.copy()
@@ -147,13 +319,50 @@ class TPerspective:
 
             cont += 1
 
+            # Generación y clasificación de lineas
             verticals, horizontals = self.get_lines(transformed, 120)
+            verticals_filtered = self.get_classified_lines_by_rho(verticals, self.vertical_tolerance, transformed, False)
+            horizontals_filtered = self.get_classified_lines_by_rho(horizontals, self.horizontal_tolerance, transformed, True)
 
-            verticals_filtered = self.get_classified_lines_by_rho(verticals, 20, transformed, False)
-            horizontals_filtered = self.get_classified_lines_by_rho(horizontals, 15, transformed, True)
-            cv2.imshow(f'{cont}_Pruebas perspectiva', transformed)
+            # Imagen inversa en perspectiva
+            empty_image = np.zeros((self.tp_height, self.tp_width, 3), np.uint8)
 
+            # Dibuja lineas
+            TPerspective.draw_lines(empty_image, verticals_filtered, (255, 255, 255), 15, cont)
+            TPerspective.draw_lines(empty_image, horizontals_filtered, (255, 255, 255), 15, cont)
+
+            # Gira en caso de ser vertical.
+            if not is_horizontal:
+                empty_image = np.rot90(empty_image, k=-1)
+
+            # Detección de contornos
+            empty_image_bw = cv2.cvtColor(empty_image, cv2.COLOR_BGR2GRAY)
+            _, empty_image_bin = cv2.threshold(empty_image_bw, 100, 255, cv2.THRESH_BINARY)
+            contours, _ = cv2.findContours(empty_image_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours) > 0:
+                cv2.drawContours(empty_image, contours, -1, (0, 255, 0), 5)
+
+            # Reversa la perspectiva
+            back_perspective = cv2.warpPerspective(empty_image, m_inverse, (1919, 1079))
+
+            cv2.imshow(f'{cont}_Pruebas perspectiva', back_perspective)
             # verticals_filtered = self.filter_bad_distances(verticals_filtered, 0.5, 0.9, transformed)
+
+    @staticmethod
+    def draw_lines(image, lines: list, color: tuple, thickness: int, counter: int):
+        """
+        Dibuja lineas en la imagen
+        :param image:
+        :param lines:
+        :param color:
+        :param thickness:
+        :param counter:
+        :return:
+        """
+        for line in lines:
+            cv2.line(image, (line['x1'], line['y1']), (line['x2'], line['y2']), color, thickness)
+
+        cv2.imshow(f'{counter}_Nueva perspectiva', image)
 
     def get_classified_lines_by_rho(self, lines: list, tolerance: int, image, is_horizontal: bool):
 
@@ -177,7 +386,8 @@ class TPerspective:
                 clusters.append(base)
 
         if is_horizontal:
-            clusters = filter(lambda c: (len(c) > 5), clusters)
+            pass
+            #clusters = filter(lambda x: (len(x) > 5), clusters)
 
         # Calculate mean
         for c in clusters:
@@ -194,10 +404,10 @@ class TPerspective:
 
                     radianes_vertical = self.vertical_degrees * np.pi / 180
 
-                    if (np.pi - radianes_vertical) < theta and theta < (radianes_vertical + np.pi):
+                    if np.pi - radianes_vertical < theta < radianes_vertical + np.pi:
                         l['rho'] = -1 * l['rho']
                         l['theta'] = l['theta'] - np.pi
-                    elif (2 * np.pi - radianes_vertical) < theta and theta < radianes_vertical:
+                    elif (2 * np.pi) - radianes_vertical < theta < radianes_vertical:
                         l['theta'] = l['theta'] - (2 * np.pi)
 
                     rho += l['rho']
@@ -211,13 +421,13 @@ class TPerspective:
             x0 = a * rho
             y0 = b * rho
             x1 = int(x0 + max_size * (-b))
-            y1 = int(y0 + max_size * (a))
+            y1 = int(y0 + max_size * a)
             x2 = int(x0 - max_size * (-b))
-            y2 = int(y0 - max_size * (a))
+            y2 = int(y0 - max_size * a)
 
             # Calcula la linea equivalente
             final.append({'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2, 'rho': rho, 'theta': theta})
-            cv2.line(image,(x1,y1),(x2,y2),(255,125,125),15)
+            # cv2.line(image, (x1, y1), (x2, y2), (255, 125, 125), 15)
 
         return final
 
